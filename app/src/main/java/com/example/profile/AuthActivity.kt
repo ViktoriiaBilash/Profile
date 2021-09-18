d0df879bb7bfabd7edd7e599af14fc8f8c002704
Viktoriia Bilash <viktoriyabilash@gmail.com>
2021-09-19 01:14:40 +0300

    18.09

---
+++ app/src/main/java/com/example/profile/AuthActivity.kt
@@ -0,0 +1,13 @@
+package com.example.profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+    
+
+}
