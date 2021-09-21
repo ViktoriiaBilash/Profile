5b7ff1fe44f3b731c28e5ffe464217827c78e2c1
Viktoriia Bilash <viktoriyabilash@gmail.com>
2021-09-21 22:51:12 +0300

    21/09

--- app/src/main/java/com/example/profile/AuthActivity.kt
+++ app/src/main/java/com/example/profile/AuthActivity.kt
@@ -1,7 +1,9 @@
 package com.example.profile
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,5 +11,8 @@ class AuthActivity : AppCompatActivity() {
         setContentView(R.layout.activity_auth)
     }
     
-
+    fun onClick(view: View){
+val registerIntent = Intent(this, MainActivity::class.java)
+        startActivity(registerIntent)
+    }
 }
